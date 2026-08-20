93faf9ea32e8007fae25836bbadddc574e5c5b8a
Rubén Martínez Sánchez <ruboms@gmail.com>
2026-08-20 11:18:09 +0200

    Ejercicio 9

--- api/v1/hola.js
+++ api/v1/hola.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     res.status(200).json({
-        mensaje: "Bienvenido a la versión 1 de nuestra API"
+        mensaje: "¡API actualizada y mejorada!"
     });
 }
 
