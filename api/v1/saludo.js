62110beb16b0cc5e762790335b265cec43b2a3f3
Rubén Martínez Sánchez <ruboms@gmail.com>
2026-08-20 11:46:07 +0200

    Ejercicio 12

---
+++ api/v1/saludo.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+    const { nombre } = req.query;
+
+    if (!nombre) {
+        return res.status(400).json({
+            mensaje: "Debes proporcionar un nombre en la consulta. Ejemplo: /api/v1/saludo?nombre=Ruben"
+        });
+    }
+
+    res.status(200).json({
+        mensaje: `Hola, ${nombre}!`
+    });
+}
